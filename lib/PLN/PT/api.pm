3a511a044f501159834699271811b0127a92c727
Nuno Carvalho <mestre.smash@gmail.com>
2017-08-27 14:57:09 +0100

    Organize code around p6

--- lib/PLN/PT/api.pm
+++ lib/PLN/PT/api.pm
@@ -14,6 +14,7 @@ use PLN::PT::api::dep_parser;
 use PLN::PT::api::morph_analyzer;
 use PLN::PT::api::stopwords;
 use PLN::PT::api::actants;
+use PLN::PT::api::tf;
 
 our $VERSION = '0.1';
 
@@ -32,51 +33,7 @@ post '/tokenizer'        => \&PLN::PT::api::tokenizer::route;
 post '/tagger'           => \&PLN::PT::api::tagger::route;
 post '/dep_paser'        => \&PLN::PT::api::dep_paser::route;
 post '/actants'          => \&PLN::PT::api::actants::route;
-
-# POST /tf
-post '/tf' => sub {
-  my $text = request->body;
-  my $options = PLN::PT::api::utils::handle_opts();
-
-  my $i = File::Temp->new( DIR => $TMPDIR );
-  my $o = File::Temp->new( DIR => $TMPDIR );
-  my ($input, $output) = ($i->filename, $o->filename);
-
-  path($input)->spew_raw($text);
-  my ($raw, $json) = PLN::PT::api::tagger::tagger($input, $output, {ner => 1});
-
-  my $ct = 'application/json';
-  $ct = 'plain/text' if ($options->{output} eq 'raw');
-  content_type "$ct; charset='utf8'";
-
-  my $data = PLN::PT::api::utils::my_decode($json);
-  my $tf = {};
-  my $stopwords = _stopwords('PT');
-  foreach (@$data) {
-    my $t;
-    if ($options->{term} eq 'lemma') {
-      $t = $_->[1];
-    }
-    else {
-      $t = $_->[0];
-    }
-
-    # use form for 'conjunctions'
-    $t = $_->[0] if ($_->[2] =~ m/\+/);
-    # use form for dates
-    $t = $_->[0] if ($_->[2] =~ m/^W/);
-
-    # $t = lc($t) unless $_->[2] =~ m/^NP/; FIXME
-    next if $_->[2] =~ m/^F/;
-    if ($options->{stopwords}) {
-      next if grep {$_ eq lc($t)} @$stopwords;
-    }
-
-    $tf->{$t}++;
-  }
-
-  return PLN::PT::api::utils::my_encode($tf);
-};
+post '/tf'               => \&PLN::PT::api::tf::route;
 
 true;
 
---
+++ lib/PLN/PT/api/tf.pm
@@ -0,0 +1,59 @@
+package PLN::PT::api::tf;
+use Dancer2 appname => 'PLN::PT::api';
+use File::Temp;
+use Path::Tiny;
+
+use PLN::PT::api::utils;
+use PLN::PT::api::tagger;
+
+# env configuration
+my $TMPDIR = config->{'TMPDIR'};
+my $FL4BIN = config->{'FL4BIN'};
+my $FL4CFG = config->{'FL4CFG'};
+
+sub route {
+  my $text = request->body;
+  my $options = PLN::PT::api::utils::handle_opts();
+
+  my $i = File::Temp->new( DIR => $TMPDIR );
+  my $o = File::Temp->new( DIR => $TMPDIR );
+  my ($input, $output) = ($i->filename, $o->filename);
+
+  path($input)->spew_raw($text);
+  my ($raw, $json) = PLN::PT::api::tagger::tagger($input, $output, {ner => 1});
+
+  my $ct = 'application/json';
+  $ct = 'plain/text' if ($options->{output} eq 'raw');
+  content_type "$ct; charset='utf8'";
+
+  my $data = PLN::PT::api::utils::my_decode($json);
+  my $tf = {};
+  my $stopwords = _stopwords('PT');
+  foreach (@$data) {
+    my $t;
+    if ($options->{term} eq 'lemma') {
+      $t = $_->[1];
+    }
+    else {
+      $t = $_->[0];
+    }
+
+    # use form for 'conjunctions'
+    $t = $_->[0] if ($_->[2] =~ m/\+/);
+    # use form for dates
+    $t = $_->[0] if ($_->[2] =~ m/^W/);
+
+    # $t = lc($t) unless $_->[2] =~ m/^NP/; FIXME
+    next if $_->[2] =~ m/^F/;
+    if ($options->{stopwords}) {
+      next if grep {$_ eq lc($t)} @$stopwords;
+    }
+
+    $tf->{$t}++;
+  }
+
+  return PLN::PT::api::utils::my_encode($tf);
+};
+
+true;
+
